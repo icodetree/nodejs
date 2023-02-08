526a506ba6a3c33e139b8703c4b6885e7ed6d466
icodetree <icodetree@naver.com>
2023-02-09 01:01:12 +0900

    add api setting

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-var express = require ('express');
-var app = express ();
+const express = require ('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get ('/', function (req, res) {
   res.send ('Hello World!');
+});
+
+app.get ('/sound/:name', (req, res) => {
+	const { name } = req.params;
+	
+	if (name == "dog") {
+		res.json ({ 'sound' : '멍멍'});		
+	} else if (name == "cat") {
+		res.json ({ 'sound' : '야옹'});		
+	}else if (name == "pig") {
+		res.json ({ 'sound' : '꿀꿀'});		
+	} else {
+		res.json({ 'sound' : '알수없음' })
+	}
 });
 
 app.listen (3000);
