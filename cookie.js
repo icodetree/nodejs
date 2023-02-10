52c522a6a81da6c9caaa995b0885ef5c3c108fd0
icodetree <icodetree@naver.com>
2023-02-10 22:38:03 +0900

    add session

---
+++ cookie.js
@@ -0,0 +1,11 @@
+const http = require ('http');
+
+http
+  .createServer ((req, res) => {
+    res.writeHead (200, {'Set-cookie': 'name=roadbook'});
+    console.log (req.headers.cookie);
+    res.end ('Cookie ===> Header');
+  })
+  .listen (8080, () => {
+    console.log ('8080 포트에서 서버 연결중...');
+  });
--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const express = require ('express');
-const app = express();
-const cors = require('cors');
+const app = express ();
 
-app.use(cors());
-
-app.get ('/', function (req, res) {
-  res.send ('Hello World!');
+app.set ('port', process.env.PORT || 8080);
+app.use (express.static (__dirname + '/public'));
+app.get ('/', (req, res) => {
+  res.sendFile (__dirname + '/index2.html');
 });
-
-app.get ('/sound/:name', (req, res) => {
-	const { name } = req.params;
-	
-	if (name == "dog") {
-		res.json ({ 'sound' : '멍멍'});		
-	} else if (name == "cat") {
-		res.json ({ 'sound' : '야옹'});		
-	}else if (name == "pig") {
-		res.json ({ 'sound' : '꿀꿀'});		
-	} else {
-		res.json({ 'sound' : '알수없음' })
-	}
+app.listen (app.get ('port'), () => {
+  console.log (app.get ('port'), '번 포트에서서버 실행중...');
 });
-
-app.listen (3000);
