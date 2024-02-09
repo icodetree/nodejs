1e53178afee8eb474240cba1fe505c3feaef2d26
icodetree <icodetree@naver.com>
2024-02-09 18:12:31 +0900

    add jobkorea mailler

--- crawlings/jobkorea.js
+++ crawlings/jobkorea.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+require("dotenv").config({ path: "nodemailer/.env" });
+const nodemailer = require("./modules/nodemailer");
 
 const getHTML = async (keyword) => {
   try {
@@ -28,16 +30,20 @@ const parsing = async (page) => {
     const dueDate = $(node).find(".date:eq(0)").text().trim();
     const etc = $(node).find(".etc:eq(0)").text().trim();
 
-    jobs.push({
-      jobTitle,
-      company,
-      experience,
-      education,
-      regularYN,
-      region,
-      dueDate,
-    });
-    console.log(etc);
+    if (
+      experience.indexOf("신입") > -1 ||
+      experience.indexOf("경력무관") > -1
+    ) {
+      jobs.push({
+        jobTitle,
+        company,
+        experience,
+        education,
+        regularYN,
+        region,
+        dueDate,
+      });
+    }
   });
 
   return jobs;
@@ -48,6 +54,50 @@ const getJob = async (keyword) => {
   const jobs = await parsing(html);
 
   console.log(jobs);
+  return jobs;
 };
 
-getJob("node.js");
+const crawlingJob = async (keyword) => {
+  const jobs = await getJob(keyword);
+
+  const h = [];
+  h.push(`<table style="border:1px solid black;border-collapse:collapse;">`);
+  h.push(`<thead>`);
+  h.push(`<tr>`);
+  h.push(`<th>구인제목</th>`);
+  h.push(`<th>회사명</th>`);
+  h.push(`<th>경력</th>`);
+  h.push(`<th>학력</th>`);
+  h.push(`<th>정규직여부</th>`);
+  h.push(`<th>지역</th>`);
+  h.push(`<th>구인마감일</th>`);
+  h.push(`<th>비고</th>`);
+  h.push(`</th>`);
+  h.push(`</thead>`);
+  h.push(`<tbody>`);
+  jobs.forEach((job) => {
+    h.push(`<tr>`);
+    h.push(`<td>${job.jobTitle}</td>`);
+    h.push(`<td>${job.company}</td>`);
+    h.push(`<td>${job.experience}</td>`);
+    h.push(`<td>${job.education}</td>`);
+    h.push(`<td>${job.regularYN}</td>`);
+    h.push(`<td>${job.region}</td>`);
+    h.push(`<td>${job.dueDate}</td>`);
+    h.push(`<td>${job.etc}</td>`);
+    h.push(`</td>`);
+  });
+  h.push(`</tbody>`);
+  h.push(`</table>`);
+
+  const emailData = {
+    from: "master8407@naver.com",
+    to: "bhy8407@gmail.com",
+    subject: "Node.js 구인 회사 정보",
+    html: h.join(""),
+  };
+
+  await nodemailer.send(emailData);
+};
+
+crawlingJob("웹퍼블리셔");
